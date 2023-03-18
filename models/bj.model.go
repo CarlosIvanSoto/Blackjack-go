997f92595a66aad661efb17f2041db599ada9454
Carlos Ivan Soto <gameue@hotmail.com>
2023-03-18 10:56:43 -0600

    version funcional

---
+++ models/bj.model.go
@@ -0,0 +1,27 @@
+package models
+
+type Player struct {
+	Id    int
+	Name  string
+	Coins int
+}
+type Hand struct {
+	Cards []Card
+}
+type Card struct {
+	Rank int
+	Suit int
+}
+type Deck struct {
+	Cards []Card
+}
+type Blackjack struct {
+	Message         string
+	Deck            *Deck
+	DealerHand      Hand
+	IndexPlayerTurn int
+	Players         []Player
+	Hands           []Hand
+	IsGameOver      bool
+	bet             []int
+}
